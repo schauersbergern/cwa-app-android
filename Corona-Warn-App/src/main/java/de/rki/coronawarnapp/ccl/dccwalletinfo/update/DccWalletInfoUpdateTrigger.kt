7d06cd0d368e558889d4750cdf48d6a7b2619e58
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-02-18 16:23:36 +0100

    Try automatic trigger

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/update/DccWalletInfoUpdateTrigger.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/update/DccWalletInfoUpdateTrigger.kt
@@ -2,15 +2,35 @@ package de.rki.coronawarnapp.ccl.dccwalletinfo.update
 
 import de.rki.coronawarnapp.ccl.dccwalletinfo.update.DccWalletInfoUpdateTask.DccWalletInfoUpdateTriggerType.TriggeredAfterCertificateChange
 import de.rki.coronawarnapp.ccl.dccwalletinfo.update.DccWalletInfoUpdateTask.DccWalletInfoUpdateTriggerType.TriggeredAfterConfigUpdate
+import de.rki.coronawarnapp.covidcertificate.person.core.PersonCertificatesProvider
 import de.rki.coronawarnapp.tag
 import de.rki.coronawarnapp.task.TaskController
 import de.rki.coronawarnapp.task.common.DefaultTaskRequest
+import de.rki.coronawarnapp.util.coroutine.AppScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
 import timber.log.Timber
 import javax.inject.Inject
 
 class DccWalletInfoUpdateTrigger @Inject constructor(
-    private val taskController: TaskController
+    personCertificatesProvider: PersonCertificatesProvider,
+    private val taskController: TaskController,
+    @AppScope val appScope: CoroutineScope
 ) {
+
+    init {
+        personCertificatesProvider.personCertificates
+            .zipWithNext { prevPersons, currentPersons ->
+                val prevsCerts = prevPersons.flatMap { it.certificates.map { it.qrCodeHash } }.sorted().toSet()
+                val currentCerts = currentPersons.flatMap { it.certificates.map { it.qrCodeHash } }.sorted().toSet()
+                if (prevsCerts != currentCerts) {
+                    triggerDccWalletInfoUpdateAfterCertificateChange()
+                }
+            }.launchIn(appScope)
+    }
 
     fun triggerDccWalletInfoUpdateAfterConfigUpdate(configurationChanged: Boolean = false) {
         Timber.tag(TAG).d("triggerDccWalletInfoUpdateAfterConfigUpdate()")
@@ -42,3 +62,13 @@ class DccWalletInfoUpdateTrigger @Inject constructor(
         private val TAG = tag<DccWalletInfoUpdateTrigger>()
     }
 }
+
+fun <T, R> Flow<T>.zipWithNext(transform: (T, T) -> R): Flow<R> = flow {
+    var prev: Any? = UNDEFINED
+    collectLatest { value ->
+        if (prev !== UNDEFINED) emit(transform(prev as T, value))
+        prev = value
+    }
+}
+
+private object UNDEFINED
