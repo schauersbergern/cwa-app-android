247e6cfef0b603d60ad35b2b346e0ff22445337f
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-10 20:59:24 +0100

    Fix compile error

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/keydownload/ui/KeyDownloadTestFragmentViewModel.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/keydownload/ui/KeyDownloadTestFragmentViewModel.kt
@@ -2,42 +2,39 @@ package de.rki.coronawarnapp.test.keydownload.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.diagnosiskeys.download.KeyPackageSyncTool
 import de.rki.coronawarnapp.diagnosiskeys.storage.KeyCacheRepository
 import de.rki.coronawarnapp.storage.TestSettings
-import de.rki.coronawarnapp.util.coroutine.AppScope
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.network.NetworkStateProvider
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.sample
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class KeyDownloadTestFragmentViewModel @AssistedInject constructor(
     dispatcherProvider: DispatcherProvider,
     networkStateProvider: NetworkStateProvider,
-    @AppScope appScope: CoroutineScope,
     private val testSettings: TestSettings,
     private val keyPackageSyncTool: KeyPackageSyncTool,
     private val keyCacheRepository: KeyCacheRepository,
 ) : CWAViewModel(dispatcherProvider = dispatcherProvider) {
 
-    val currentCache = appScope.launch {
-        keyCacheRepository.allCachedKeys()
-            .sample(250)
-            .map { items ->
-                items
-                    .sortedWith(compareBy({ it.info.day }, { it.info.hour }))
-                    .reversed()
-                    .map { CachedKeyListItem(it.info, it.path.length()) }
-            }
-            .asLiveData()
+    val currentCache = liveData {
+        emitSource(
+            keyCacheRepository.allCachedKeys().sample(250)
+                .map { items ->
+                    items
+                        .sortedWith(compareBy({ it.info.day }, { it.info.hour }))
+                        .reversed()
+                        .map { CachedKeyListItem(it.info, it.path.length()) }
+                }.asLiveData2()
+        )
     }
 
     val isMeteredConnection = networkStateProvider.networkState
